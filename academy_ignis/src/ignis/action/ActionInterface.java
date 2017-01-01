1d9900d423d5fd31f728abdb9dc546a47802c9be
Shim.hw <shim.hw0810@gmail.com>
2017-01-01 14:17:54 +0900

    메인 서블릿과 포워드클래스 기초 양식 구성

---
+++ academy_ignis/src/ignis/action/ActionInterface.java
@@ -0,0 +1,8 @@
+package ignis.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface ActionInterface {
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
+}
---
+++ academy_ignis/src/ignis/servlet/FrontController.java
@@ -0,0 +1,67 @@
+package ignis.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ignis.action.ActionInterface;
+import ignis.action.ActionForward;
+
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FrontController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doProcess(request, response);
+	}
+	
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws SecurityException, IOException {
+		String RequestURI = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = RequestURI.substring(contextPath.length());
+		
+		ActionForward forward = null;
+		ActionInterface action = null;
+		
+		switch (command) {
+		
+		case "" :
+			forward = new ActionForward();
+			forward.setRedirect(false);
+			forward.setPath("./");
+			break;
+//		case "/" :
+//			action = new Action();
+//			
+//			try {
+//				forward = action.execute(request, response);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//			break;
+//		default:
+//			if (forward != null) {
+//				if (forward.isRedirect()) {
+//					response.sendRedirect(forward.getPath());
+//				} else {
+//					RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+//					dispatcher.forward(request, response);
+//				}
+//			}
+//			break;
+		}
+		
+	}
+}
