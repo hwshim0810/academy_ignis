f7d049ebfd8d1bd6df74b64a70aacf241d5b7224
dundundawn <lsy.dawnlee@gmail.com>
2017-01-05 11:20:50 +0900

    notice애서 상세보기 기능 count+1 목록 돌아오기 페이지 기능 추가완료

--- academy_ignis/src/ignis/action/NoticeDetailAction.java
+++ academy_ignis/src/ignis/action/NoticeDetailAction.java
@@ -15,37 +15,25 @@ public class NoticeDetailAction  implements ActionInterface{
 		ActionForward forward = new ActionForward();
 		
 		int pagenum=1;//현재 페이지
-		int pagelimit=10;//한 페이지 리스트수
-		int pageCount=10;//마지막 페이지
-		int startpage=1;//페이지 범위
-		int endpage=10;//페이지 범위
 		if(request.getParameter("pagenum")!=null){
 			pagenum=Integer.parseInt(request.getParameter("pagenum"));
-			System.out.println("pagenum을 request에서 가져옴:"+request.getParameter("pagenum"));
 		}
-		
-		int startRowNum=(pagenum-1)*pagelimit+1;//가져오는 리스트
-		int endRowNum=startRowNum+pagelimit-1;//가져오는 리스트
-		int listCount=noticeBiz.getListcount();//총 리스트 수
-		if(listCount%pagelimit==0){
-			pageCount = listCount/pagelimit;//총 리스트수로 페이지수 계산
-		}else{
-			pageCount = listCount/pagelimit+1;
+		int nb_num=1;//상세정보 요청 number
+		if(request.getParameter("nb_num")!=null){
+			nb_num=Integer.parseInt(request.getParameter("nb_num"));
 		}
-		startpage= ((pagenum-1)/10)*10+1;
-		endpage=startpage+10-1;
+		int readCount_change=noticeBiz.updateCount(nb_num);//조회수 +1
+		List<ig_notice> list = noticeBiz.selectDetail(nb_num);//상세 정보 가져옴
 		
-		List<ig_notice> list = noticeBiz.selectAll(startRowNum,endRowNum);
+		
+		if(readCount_change>0){System.out.println("readcount +1 성공");
+		}else{System.out.println("readcount +1 실패");}
+		
 		request.setAttribute("pagenum", pagenum);
-		request.setAttribute("pagelimit", pagelimit);
-		request.setAttribute("pageCount", pageCount);
-		request.setAttribute("startpage", startpage);
-		request.setAttribute("endpage", endpage);
-    	System.out.println(pagenum+" ,"+pagelimit+" ,"+pageCount+" ,"+startpage+" ,"+endpage);
 		if (list !=null) {
-			request.setAttribute("noticelist", list);
+			request.setAttribute("noticeDetail", list);
 			forward.setRedirect(false);
-			forward.setPath("./community/notice.jsp");
+			forward.setPath("./community/noticeDetail.jsp");
 			return forward;
 		}
 		
