ffd02e9db5b3887f5f8daf1c03acdbd3d9ec268a
qqqq1230qqqq <qqqq1230qqqq@DESKTOP-C6GDP7T>
2017-01-10 22:09:41 +0900

    review게시판 끝~~ Clients 혜경

--- academy_ignis/src/ignis/action/ReviewDeleteAction.java
+++ academy_ignis/src/ignis/action/ReviewDeleteAction.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ignis.biz.ReviewBiz;
+import ignis.dao.ReviewDAO;
 
 
 public class ReviewDeleteAction implements ActionInterface {
@@ -13,9 +14,29 @@ public class ReviewDeleteAction implements ActionInterface {
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		ReviewBiz reviewBiz = new ReviewBiz();
+		ReviewDAO reviewDao = new ReviewDAO();
 		ActionForward forward = new ActionForward();
 		int num = Integer.valueOf(request.getParameter("num"));
 		String login = request.getParameter("login");
+		String pageNo = "1";
+		
+		if(request.getParameter("pageNo") != null){
+			pageNo = request.getParameter("pageNo");
+		}
+		
+		int commentCount = reviewDao.getListCommentCount(num);
+		if(commentCount > 0) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter outR = response.getWriter();
+			outR.println("<script>");
+			outR.println("alert('댓글이 있는 글은 삭제할 수 없습니다!');");
+			outR.println("location.href='/academy_ignis/ReviewView?login=member&pageNo=" + pageNo + "&num="+ num + "&commPageNo=1';");
+			outR.println("</script>");
+			outR.close();
+			
+			return null;
+		}
+		
 		boolean result = reviewBiz.deleteReview(request, response);
 		
 		if(login.equals("admin")){
